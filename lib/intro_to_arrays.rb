6bf888575cc363d74dd6c10659b1bc2b648ed9a7
Andrew Allen <asallen67@gmail.com>
2019-04-07 20:55:47 -0500

    1

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+	return []
+end
+
+def array_with_two_elements
+	return [1,2]
+end
+
+def first_element(arr)
+	return arr[0]
+end
+
+def third_element(arr)
+	return arr[2]
+end
+
+def last_element(arr)
+	return arr[arr.size-1]
+end
+
+def first_element_with_array_methods(arr)
+	return first_element(arr)
+end
+
+def last_element_with_array_methods(arr)
+	return last_element(arr)
+end
+
+def length_of_array(arr)
+	return arr.size
+end
